94f7583b3c7ae37f51c36185549701bd07bfd765
Tengo Meshveliani <meshvelianitengo@gmail.com>
2017-01-03 19:39:16 +0400

    barcode reader damatebulia

--- MyApplication/app/src/main/java/menu/com/voli/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/menu/com/voli/myapplication/MainActivity.kt
@@ -1,12 +1,75 @@
 package menu.com.voli.myapplication
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPremissions()
+        text_view.setOnClickListener { checkPremissions() }
+//        IntentIntegrator(this).initiateScan();
     }
+
+     val MY_PERMISSIONS_REQUEST_READ_CONTACTS: Int = 19
+
+    private fun checkPremissions(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        arrayOf<String>(Manifest.permission.CAMERA),
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }else{
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode === Activity.RESULT_OK) {
+
+            val _code = data?.getStringExtra("SCAN_RESULT")
+            Toast.makeText(this, "code = "  + _code, Toast.LENGTH_SHORT).show()
+            System.out.println("_code = " + _code)
+            // do whatever you want
+
+        }
+
+    }
+
 }
